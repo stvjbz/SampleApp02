65891cb1a377a116e13784db40979040a79bb727
stvjbz <stvjbz@outlook.com>
2016-05-02 18:30:09 +0900

    [add] MainActivityにボタン押下時のイベントリスナーを設定。
    押下時に明示的インテントを用い、ボタン毎に指定のActivityを開くように設定。

--- app/src/main/java/com/stvjbz/sampleapp02/MainActivity.java
+++ app/src/main/java/com/stvjbz/sampleapp02/MainActivity.java
@@ -1,7 +1,10 @@
 package com.stvjbz.sampleapp02;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnPickUp = (Button) this.findViewById(R.id.button1);
+        btnPickUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PickUpActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button btnNoDinner = (Button) this.findViewById(R.id.button2);
+        btnNoDinner.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NoDinnerActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
