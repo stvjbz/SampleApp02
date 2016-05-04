f9c8b9568fdde5d6d477051d11350c5cde5cc804
stvjbz <stvjbz@outlook.com>
2016-05-04 14:22:01 +0900

    [add] PickUpActivityにボタン押下時のイベントリスナーを設定。
    押下時に明示的インテントを用い、メール送信アクションを設定。

--- app/src/main/java/com/stvjbz/sampleapp02/PickUpActivity.java
+++ app/src/main/java/com/stvjbz/sampleapp02/PickUpActivity.java
@@ -1,7 +1,16 @@
 package com.stvjbz.sampleapp02;
 
+import android.content.Intent;
+import android.content.res.Resources;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class PickUpActivity extends AppCompatActivity {
 
@@ -9,5 +18,25 @@ public class PickUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pick_up);
+        Button btnSend = (Button) this.findViewById(R.id.button);
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RadioGroup rgPlace = (RadioGroup) findViewById(R.id.rg_place);
+                int checkedId = rgPlace.getCheckedRadioButtonId();
+                String strPlace = ((RadioButton) findViewById(checkedId)).getText().toString();
+                Log.d("Place:", strPlace);
+                EditText edit01 = (EditText) findViewById(R.id.editText);
+                String title = edit01.getText().toString();
+                Resources res = getResources();
+                Uri uri = Uri.parse("mailto:" + res.getString(R.string.mail_to).toString());
+
+                Intent intent = new Intent(Intent.ACTION_SENDTO, uri);
+                intent.putExtra(Intent.EXTRA_SUBJECT, title);
+                intent.putExtra(Intent.EXTRA_TEXT, strPlace + "迎えに来て");
+                startActivity(intent);
+            }
+        });
     }
 }
